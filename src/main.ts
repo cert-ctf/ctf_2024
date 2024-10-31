acee473fe467ee1255848e2cba0d83f94b833939
cert-ctf <tobias.kirschke@gematik.de>
2024-10-31 10:25:10 +0100

    update disable room list

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
+	WA.controls.disableRoomList();
 	
 	// Mirror show Duke
 	WA.room.area.onEnter('Mirror_Duke').subscribe(() => {		
